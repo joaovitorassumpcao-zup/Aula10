e9f6d5f08882ec0db90b8acc329b6fddbeacba7d
joaovitorassumpcao-zup <joao.assumpcao@zup.com.br>
2023-05-29 20:18:31 -0300

    Estrutura de Repeticao Entrega: Exercicio 6

--- src/EstruturaDeRepeticao_entregar/Exer6.java
+++ src/EstruturaDeRepeticao_entregar/Exer6.java
@@ -1,4 +1,26 @@
 package EstruturaDeRepeticao_entregar;
 
+import java.util.Scanner;
+
 public class Exer6 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Número à ser verificado: ");
+        int num = scanner.nextInt();
+
+        if (num <= 1) {
+            System.out.println("Não é primo");
+            return;
+        }
+
+        for (int i = 2; i < num; i++) {
+            if (num % i == 0) {
+                System.out.println("Não é primo");
+                return;
+            }
+        }
+
+        System.out.println("É primo!");
+    }
 }
